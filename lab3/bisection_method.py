f24dca79897fc0c17128b25210c37c28760e145a
FJ05 <fredrikfilip.fj@gmail.com>
2024-10-11 23:51:13 +0200

    finnished lab3

--- lab3/bisection_method.py
+++ lab3/bisection_method.py
@@ -1,5 +1,38 @@
-def bisection_solver(x:int,a:int,b:int,c:int,d:int):
+def f(x:float,a:float,b:float,c:float,d:float):
     return (a*(x**3) + b*(x**2) + c*x + d)
 
+def l(a:float,b:float,c:float,d:float):
+    l = 0
+    while f(l,a,b,c,d) >= 0:
+        l -= 1
+    return l
+
+def r(a:float,b:float,c:float,d:float):
+    r = 0
+    while f(r,a,b,c,d) <= 0:
+        r =+ 1
+    return r
+
+def bisection_method(l,r,f:float,t, coef:float):
+    while (r, - 1) / 2.0 > t:
+        c = (l + r) / 2.0
+        fc = f(c, *coef)
+
+        if abs(fc) <=  t:
+            return c
+        elif f(l, *coef) * fc < 0:
+            r = c
+        else:
+            l = c
+    return (l + r) / 2.0
+
 print("Enter the values for:\nf(x) = ax³ + bx² + cx + d\nto find f(x)")
-print(bisection_solver(int(input("Enter the value for x: ")), int(input("Enter the value for a: ")), int(input("Enter the value for b: ")), int(input("Enter the value for c: ")), int(input("Enter the value for d: "))))
+x,a,b,c,d = float(input("Enter the value for x: ")), float(input("Enter the value for a: ")), float(input("Enter the value for b: ")), float(input("Enter the value for c: ")), float(input("Enter the value for d: ")))
+
+print(f"f({x}) = {f(x,a,b,c,d)}")
+l = l(a,b,c,d)
+print(f"f({l}) < 0")
+r = r(a,b,c,d)
+print(f"f({r}) > 0")
+aprox = bisection_method(l,r,f,0.001,(a,b,c,d))
+print(f"An approximate solution of f(x) = 0 is f({aprox})\nf({aprox}) = {f(aprox, a,b,c,d)}")
