b64cccb189549d5aebbe2dd6dd27a86d71b0b0fb
FJ05 <fredrikfilip.fj@gmail.com>
2024-10-14 20:03:28 +0200

    fixed b_m

--- lab3/bisection_method.py
+++ lab3/bisection_method.py
@@ -1,24 +1,24 @@
-def f(x:float,a:float,b:float,c:float,d:float):
-    return (a*(x**3) + b*(x**2) + c*x + d)
+def f(x: float, a: float, b: float, c: float, d: float):
+    return a*(x**3) + b*(x**2) + c*x + d
 
-def l(a:float,b:float,c:float,d:float):
+def l(a: float, b: float, c: float, d: float):
     l = 0
-    while f(l,a,b,c,d) >= 0:
+    while f(l, a, b, c, d) >= 0:
         l -= 1
     return l
 
-def r(a:float,b:float,c:float,d:float):
+def r(a: float, b: float, c: float, d: float):
     r = 0
-    while f(r,a,b,c,d) <= 0:
-        r =+ 1
+    while f(r, a, b, c, d) <= 0:
+        r += 1
     return r
 
-def bisection_method(l,r,f:float,t, coef:float):
-    while (r, - 1) / 2.0 > t:
+def bisection_method(l, r, f, t, coef):
+    while abs(r - l) / 2.0 > t:
         c = (l + r) / 2.0
         fc = f(c, *coef)
 
-        if abs(fc) <=  t:
+        if abs(fc) <= t:
             return c
         elif f(l, *coef) * fc < 0:
             r = c
@@ -27,12 +27,12 @@ def bisection_method(l,r,f:float,t, coef:float):
     return (l + r) / 2.0
 
 print("Enter the values for:\nf(x) = ax³ + bx² + cx + d\nto find f(x)")
-x,a,b,c,d = float(input("Enter the value for x: ")), float(input("Enter the value for a: ")), float(input("Enter the value for b: ")), float(input("Enter the value for c: ")), float(input("Enter the value for d: "))
+x, a, b, c, d = float(input("Enter the value for x: ")), float(input("Enter the value for a: ")), float(input("Enter the value for b: ")), float(input("Enter the value for c: ")), float(input("Enter the value for d: "))
 
-print(f"f({x}) = {f(x,a,b,c,d)}")
-l = l(a,b,c,d)
-print(f"f({l}) < 0")
-r = r(a,b,c,d)
-print(f"f({r}) > 0")
-aprox = bisection_method(l,r,f,0.001,(a,b,c,d))
-print(f"An approximate solution of f(x) = 0 is f({aprox})\nf({aprox}) = {f(aprox, a,b,c,d)}")
+print(f"f({x}) = {f(x, a, b, c, d)}")
+l_val = l(a, b, c, d)
+print(f"f({l_val}) < 0")
+r_val = r(a, b, c, d)
+print(f"f({r_val}) > 0")
+aprox = bisection_method(l_val, r_val, f, 0.001, (a, b, c, d))
+print(f"An approximate solution of f(x) = 0 is f({aprox})\nf({aprox}) = {f(aprox, a, b, c, d)}")
